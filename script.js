bd2d2302a68243a0d4186177a63dcbe0006446cd
ykyritsis <102499171+ykyritsis@users.noreply.github.com>
2024-05-31 10:23:53 +1000

    Add files via upload

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const scrollToOptionsButton = document.getElementById('scroll-to-options');
     const optionsSection = document.getElementById('options-section');
     const optionDescription = document.getElementById('option-description');
+    const contactButton = document.getElementById('contact-button');
+    const customCursor = document.getElementById('custom-cursor');
 
     const originalText = "As a third-year computer science student with a passion for web development and artificial intelligence,<br>I am currently advancing my skills as a data scientist. I am dedicated to exploring the frontiers of technology and crafting innovative solutions that enhance user experiences.";
 
@@ -31,6 +33,7 @@ document.addEventListener('DOMContentLoaded', () => {
             setTimeout(() => {
                 optionDescription.innerHTML = option.getAttribute('data-info');
                 optionDescription.classList.add('visible');
+                optionDescription.scrollIntoView({ behavior: 'smooth' });
             }, 300);
         });
     });
@@ -57,4 +60,32 @@ document.addEventListener('DOMContentLoaded', () => {
     scrollToOptionsButton.addEventListener('click', () => {
         optionsSection.scrollIntoView({ behavior: 'smooth' });
     });
+
+    contactButton.addEventListener('click', (event) => {
+        event.preventDefault();
+        document.body.classList.add('fade-out');
+        setTimeout(() => {
+            window.location.href = 'contact.html';
+        }, 500); // Duration of the fade-out animation
+    });
+
+    // Custom cursor movement and hover effect
+    document.addEventListener('mousemove', (e) => {
+        customCursor.style.left = `${e.clientX}px`;
+        customCursor.style.top = `${e.clientY}px`;
+    });
+
+    // Only show custom cursor on desktop
+    if (window.innerWidth > 768) {
+        document.querySelectorAll('a, button, .option').forEach((element) => {
+            element.addEventListener('mouseover', () => {
+                customCursor.classList.add('hover');
+            });
+            element.addEventListener('mouseout', () => {
+                customCursor.classList.remove('hover');
+            });
+        });
+    } else {
+        customCursor.style.display = 'none';
+    }
 });
