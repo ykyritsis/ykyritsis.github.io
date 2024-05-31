7c5fa8e025545d4cae0e46de3cca730bc07233d9
ykyritsis <102499171+ykyritsis@users.noreply.github.com>
2024-05-31 10:51:59 +1000

    Add files via upload

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const optionsSection = document.getElementById('options-section');
     const optionDescription = document.getElementById('option-description');
     const contactButton = document.getElementById('contact-button');
-    const customCursor = document.getElementById('custom-cursor');
 
     const originalText = "As a third-year computer science student with a passion for web development and artificial intelligence,<br>I am currently advancing my skills as a data scientist. I am dedicated to exploring the frontiers of technology and crafting innovative solutions that enhance user experiences.";
 
@@ -68,24 +67,4 @@ document.addEventListener('DOMContentLoaded', () => {
             window.location.href = 'contact.html';
         }, 500); // Duration of the fade-out animation
     });
-
-    // Custom cursor movement and hover effect
-    document.addEventListener('mousemove', (e) => {
-        customCursor.style.left = `${e.clientX}px`;
-        customCursor.style.top = `${e.clientY}px`;
-    });
-
-    // Only show custom cursor on desktop
-    if (window.innerWidth > 768) {
-        document.querySelectorAll('a, button, .option').forEach((element) => {
-            element.addEventListener('mouseover', () => {
-                customCursor.classList.add('hover');
-            });
-            element.addEventListener('mouseout', () => {
-                customCursor.classList.remove('hover');
-            });
-        });
-    } else {
-        customCursor.style.display = 'none';
-    }
 });
